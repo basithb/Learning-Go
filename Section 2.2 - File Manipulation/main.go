6629f7fe9157f2fd743b90d4c3ed057683e80817
basithb <basithbdev@gmail.com>
2023-10-28 19:47:10 +0530

    read file successfully

--- Section 2.2 - File Manipulation/main.go
+++ Section 2.2 - File Manipulation/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"frontendmasters.com/go/basics/section-2.2/fileUtils"
 )
 
 func main() {
 
-	fileUtils.ReadTextFile("go.txt")
+	rootPath, _ := os.Getwd() // gets the root directory and stores it in the 'rootPath' variable, we don't really require the returned error value from the Getwd() method so let's store it in '_'
+
+	fmt.Println(rootPath)
+
+	content, err := fileUtils.ReadTextFile(rootPath + "/data/go.txt")
+
+	if err != nil {
+		fmt.Printf("Error %v", err)
+	} else {
+		fmt.Println(content)
+	}
 }
